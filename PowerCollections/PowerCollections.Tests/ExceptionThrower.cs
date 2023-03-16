6ba2baaab2c20287998785d6b4b5960d55f761d7
lckish <webra02@bk.ru>
2023-03-16 05:48:01 +0400

    Добавлена ошибка, чтобы не прошли тесты

---
+++ PowerCollections/PowerCollections.Tests/ExceptionThrower.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Wintellect.PowerCollections.Tests
+{
+	[TestClass]
+	public class ExceptionThrower
+	{
+		[TestMethod]
+		public void ThrowException()
+		{
+			throw new ArgumentNullException();
+		}
+	}
+}
